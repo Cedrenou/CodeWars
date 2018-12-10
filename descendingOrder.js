a789fd7cec81475733d28c73e4af8217d6628bd2
Cedric Renouleau <cedricrenouleau@MacBook-Pro-de-Cedric.local>
2018-12-10 18:03:28 +0100

    descending order

---
+++ descendingOrder.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/descending-order/train/javascript
+// Descending Order
+
+function descendingOrder(n) {
+    let arr = []
+    arr.push(n.toString().split('').sort((a, b) => b - a).join(''));
+    return parseInt(arr[0])
+}
+
+descendingOrder(21445); // 54421
