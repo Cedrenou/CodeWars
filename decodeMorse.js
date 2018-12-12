18e3e142c10adca6eb9d87e11e53433c56f7d179
Cedric Renouleau <cedricrenouleau@MacBook-Pro-de-Cedric.local>
2018-12-12 11:14:21 +0100

    :fire: :fire: :fire:

---
+++ decodeMorse.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/decode-the-morse-code/train/javascript
+// Decode the morse code
+
+// Code illisible, mais sur une ligne et ca c'est la classe :D
+
+function decodeMorse(morseCode) {
+    //your code here
+    return morseCode.trim().split('   ').map(word => word.split(' ').map(letter => MORSE_CODE[letter]).join('')).join(' ')
+}
+
